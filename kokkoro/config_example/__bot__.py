e434ea51e1c30494f97ec3f8746a91a4abc81442
zzbslayer <zzbslayer@sjtu.edu.cn>
2020-08-15 23:12:42 +0800

    fix: move BROADCAST_CHANNEL to discord config

--- kokkoro/config_example/__bot__.py
+++ kokkoro/config_example/__bot__.py
@@ -17,8 +17,6 @@ BOT_ID = platform_config.BOT_ID
 SUPER_USER = platform_config.SUPER_USER
 ENABLED_GROUP = platform_config.ENABLED_GROUP
 
-BROADCAST_CHANNEL="broadcast"
-
 LOG_LEVEL="DEBUG"
 ENABLE_IMAGE=True
 
