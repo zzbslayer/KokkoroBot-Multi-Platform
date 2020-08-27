97e68e1974b85d05f6b4434ef169008a94b38aa2
zzbslayer <zzbslayer@sjtu.edu.cn>
2020-08-27 16:06:17 +0800

    fix: remove / for gacha; prepare for unified command prefix

--- kokkoro/modules/botmanage/status.py
+++ kokkoro/modules/botmanage/status.py
@@ -4,7 +4,7 @@ from kokkoro.common_interface import KokkoroBot, EventInterface
 
 sv = Service('status', use_priv=priv.SUPERUSER, manage_priv=priv.SUPERUSER, enable_on_default=True, visible=False)
 
-@sv.on_fullmatch(('status'))
+@sv.on_fullmatch(('kkr-status'))
 async def broadcast(bot: KokkoroBot, ev: EventInterface):
     groups = bot.get_groups()
     num_g = len(groups)
