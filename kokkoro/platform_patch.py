ace6715ba764e84c3d40bed8bb9b92f3d50b7ea2
zzbslayer <zzbslayer@sjtu.edu.cn>
2020-09-28 15:47:47 +0800

    feat: mark platform-specific sv function by decorator

--- kokkoro/platform_patch.py
+++ kokkoro/platform_patch.py
@@ -1,3 +1,4 @@
+import kokkoro
 from kokkoro import config
 
 def process_mention_me(raw_msg:str):
@@ -25,6 +26,47 @@ def preprocess_message(ev) -> bool:
     ev.set_content(processed_msg)
     return to_me
 
+PLATFORM_FIELD = '__kkr_platform__'
+
+def discord(func):
+    func.__setattr__(PLATFORM_FIELD, ['discord'])
+    return func
+
+def tomon(func):
+    func.__setattr__(PLATFORM_FIELD, ['tomon'])
+    return func
+
+def telegram(func):
+    func.__setattr__(PLATFORM_FIELD, ['telegram'])
+    return func
+
+def platform(pl):
+    if type(pl) == str:
+        pl = [pl]
+    def deco(func):
+        func.__setattr__(PLATFORM_FIELD, pl)
+        return func
+    return deco
+
+def is_supported(func):
+    try:
+        supported_platforms = func.__getattribute__(PLATFORM_FIELD)
+    except AttributeError as e:
+        #supported_platforms = "*"
+        return True
+    platform = config.BOT_TYPE
+    if platform in supported_platforms:
+        return True
+    return False
+
+def check_platform(on_deco):
+    def wrapper(sv_func):
+        if is_supported(sv_func):
+            return on_deco(sv_func)
+        else:
+            kokkoro.logger.warning(f'`{sv_func.__name__}` isn\'t supported in current platform')
+            return sv_func
+    return wrapper
 
 
     
